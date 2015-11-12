7f4d1bd634f19cbdf05d23bed253f9e883add68e
seemonz <seemonz@gmail.com>
2015-11-11 16:41:28 -0800

    added basic socket connections

--- index.js
+++ index.js
@@ -18,8 +18,5 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  console.log('user connected');
 });
