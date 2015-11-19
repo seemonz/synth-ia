ebee19c8eb2e1e7c291b3cdfeae194f299cdd9d3
bittertofu <vincent.sungasong@gmail.com>
2015-11-19 11:21:30 -0800

    update to latest version

--- public/entry.js
+++ public/entry.js
@@ -1,6 +1,5 @@
 (require("./javascripts/bufferLoader.js"));
 (require("./javascripts/triggerNote.js"));
-(require("./javascripts/test.js"));
 (require("./javascripts/visualization.js"));
 (require("./javascripts/client.js"));
 (require("./javascripts/modal.js"));
